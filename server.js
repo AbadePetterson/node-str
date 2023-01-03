537f9b0861b0b512bbd0137853ff0093ddf04989
AbadePetterson <abadepetterson@gmail.com>
2023-01-03 18:00:32 -0300

    crete a function for normalise route

--- server.js
+++ server.js
@@ -23,3 +23,17 @@ app.use('/', route);
 
 server.listen(port);
 console.log('API rodando na porta ' + port);
+
+function normalizePort(val) {
+    const port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        return val;
+    }
+
+    if (port >= 0){
+        return port;
+    }
+
+    return false;
+}
