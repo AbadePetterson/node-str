7157571151a19bf5b0d92860b6002fb9face1d5b
AbadePetterson <abadepetterson@gmail.com>
2023-01-09 12:03:01 -0300

    create new organization directory

--- bin/server.js
+++ bin/server.js
@@ -1,25 +1,12 @@
-// força você a escrever código correto
-'use strict'
-
 //sempre que for importar bibliotecas no node precisa usar o require
+const app = require('../src/app.js');
 const http = require('http');
 const debug = require('debug')('nodestr:server');
-const express = require('express'); //tudo que colocar entre aspas  simples sem um caminho ele vai buscar na pasta nodemodules
 
-const app = express();
 const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
 const server = http.createServer(app);
-const router = express.Router();
-
-const route = router.get('/', (req,res, next) => {
-    res.status(200).send({
-        title: "Node Store API",
-        version: "0.0.1"
-    });
-});
-app.use('/', route);
 
 server.listen(port);
 server.on('error', onError);
