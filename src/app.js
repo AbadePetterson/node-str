07f9bc2e5249388fa7d322a20ac7b5ad282b47a1
AbadePetterson <abadepetterson@gmail.com>
2023-01-14 14:27:10 -0300

    created routers

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express();
 const router = express.Router();
 
 app.use(bodyParser.json()); //falando pra aplicação que estou usando body-parser 
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const route = router.get('/', (req, res, next) => {
     res.status(200).send({
@@ -20,7 +20,21 @@ const create = router.post('/', (req, res, next) => {
     res.status(201).send(req.body); //Pegar o corpo da requisição
 });
 
+const put = router.put('/:id', (req, res, next) => { //recuperar paramentros que irão vir pela URL
+    const id = req.params.id;
+    res.status(200).send({
+         id: id, 
+         item: req.body 
+    });
+});
+
+const del = router.delete('/', (req, res, next) => {
+    res.status(200).send(req.body); //Pegar o corpo da requisição
+});
+
 app.use('/', route);
 app.use('/products', create);
+app.use('/products', put);
+app.use('/products', del);
 
 module.exports = app;
